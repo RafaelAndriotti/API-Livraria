87d55f77418e3582d7aa473904d7720b7cbb16b5
Rafael Andriotti <rafael.rebelo47@gmail.com>
2026-05-18 21:32:44 -0300

    Primeiro dia refazendo a API

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+const livros = [
+
+    {id: 1, titulo: "O Senhor dos Anéis"},
+    {id: 2, titulo: "O Hobbit"},
+    {id: 3, titulo: "O Silmarillion"}
+
+]
+
+app.get("/", (req, res) => {
+    res.status(200).send("API RODANDO");
+})
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro adicionado com sucesso");
+});
+
+export default app;
